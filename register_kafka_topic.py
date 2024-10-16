6b908f1f63a1852ae00d0b4076225dac55c2a11d
Angel Gonzalez <angelito@de.ibm.com>
2024-10-16 09:36:41 +0200

    resp

--- register_kafka_topic.py
+++ register_kafka_topic.py
@@ -5,13 +5,16 @@ import base64
 import subprocess
 
 # presto_userid     = %system docker exec ibm-lh-presto printenv PRESTO_USER
+
 command = "docker exec ibm-lh-presto printenv PRESTO_USER"
 result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
 presto_userid = result.stdout.strip()
 
-command = "docker exec ibm-lh-presto printenv PRESTO_USER"
-result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
 
 # presto_password   = %system docker exec ibm-lh-presto printenv LH_INSTANCE_SECRET
+
+command = "docker exec ibm-lh-presto printenv LH_INSTANCE_SECRET"
+result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
 presto_password   = result.stdout.strip()
 print(f"Presto user: {presto_userid} Presto password: {presto_password}")
+
