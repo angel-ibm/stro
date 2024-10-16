22c43372f175f66dbce8f2fdd6516025cd91ee9e
Angel Gonzalez <angelito@de.ibm.com>
2024-10-16 12:32:04 +0200

    stage

--- 02_init_watsonxdata.py
+++ 02_init_watsonxdata.py
@@ -82,15 +82,34 @@ def create_staging_table(wxdconnection) :
     except Exception as err:
         print(repr(err))
 
+def insert_file(wxdconnection, image_file) :
+
+    with open(image_file, 'rb') as file :
+        file_content = file.read()
+
+    encoded_file_content = base64.b64encode(file_content).decode('utf-8')
+
+    cursor = wxdconnection.cursor()
+
+    sql = '''
+        INSERT INTO 
+            iceberg_data.fits."fits-images" (filename, filebytes)
+            values (%s, %s) 
+    '''
+    try:
+        cursor.execute(sql, (image_file, encoded_file_content))
+    except Exception as err:
+        print(repr(err))
+
 #----------------------------#
 
 wxdconnection = connect_to_watsonxdata()
 
 create_staging_table(wxdconnection)
 
-# file_paths = glob.glob("./images/m31*.FITS")
-#     for image_file in sorted(file_paths):
-#         print("Inserting file: ", image_file)
-#         insert_file
+file_paths = glob.glob("./images/m31*.FITS")
+for image_file in sorted(file_paths):
+        print("Inserting file: ", image_file)
+        insert_file(wxdconnection,image_file)
 
 
