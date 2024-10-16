0bc92db62113dd1a13bfee4078dbba2918d3c477
Angel Gonzalez <angelito@de.ibm.com>
2024-10-16 10:10:05 +0200

    noidea

--- register_kafka_topic.py
+++ register_kafka_topic.py
@@ -3,14 +3,35 @@
 import requests
 import base64
 import subprocess
+import json
+from confluent_kafka.admin import AdminClient, NewTopic
+
+BROKER = 'watsonxdata:29092'
+topic_name ='angeltopic'
+schema_name = 'default'
+table_name = 'angeltable'
+num_partitions=1
+replication_factor=1
+
+admin_client = AdminClient({'bootstrap.servers': BROKER})
+topic_list = [NewTopic(topic=topic_name, num_partitions=num_partitions, replication_factor=replication_factor)]
+existing_topics = admin_client.list_topics(timeout=10).topics
+if topic_name not in existing_topics:
+    fs = admin_client.create_topics(new_topics=topic_list)
+    for topic, f in fs.items():
+        try:
+            f.result()  
+            print(f"Created Kafka topic: {topic}")
+        except Exception as e:
+            print(f"Failed to create topic {topic}: {e}")
+else:
+    print(f"Kafka topic {topic_name} already exists.")    
+
 
 # presto_userid     = %system docker exec ibm-lh-presto printenv PRESTO_USER
-
 command = "docker exec ibm-lh-presto printenv PRESTO_USER"
 result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
 presto_userid = result.stdout.strip()
-
-
 # presto_password   = %system docker exec ibm-lh-presto printenv LH_INSTANCE_SECRET
 
 command = "docker exec ibm-lh-presto printenv LH_INSTANCE_SECRET"
@@ -32,7 +53,38 @@ auth_header = {
     "Authorization" : f"Basic {credentials}",
     "X-Presto-User" : presto_userid
 }
+
 service = "/info"
 request = {}
 r = requests.get(f"{host}:{port}{api}{service}", headers=auth_header, verify=certfile)
 print(f"Reason: {r.reason} status: {r.status_code} starting: {r.json()['starting']}")
+
+
+service = "/kafka/register-topic"
+
+columns = [
+        {"name": "event_id", "type": "VARCHAR"},
+        {"name": "event_name", "type": "VARCHAR"},
+        {"name": "event_time", "type": "TIMESTAMP"}
+]
+payload = {
+        "topic_name": topic_name,
+        "kafka_broker_url": BROKER,
+        "schema_name": schema_name,
+        "table_name": table_name,
+        "partitions": num_partitions,
+        "replication_factor": replication_factor,
+        "columns" : columns
+    }
+
+response = requests.post(f"{host}:{port}{api}{service}", headers=auth_header, data=json.dumps(payload))
+        
+try :
+    if response.status_code == 200:
+        print(f"Kafka topic '{topic_name}' registered successfully!")
+    else:
+        print(f"Failed to register Kafka topic. Status Code: {response.status_code}")
+        print(f"Response: {response.text}")
+
+except Exception as e:
+    print(f"Error while registering Kafka topic: {str(e)}")
