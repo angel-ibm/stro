a671ba6bb81aaecdf45c85e3390afb4eb7932f3f
Angel Gonzalez <angelito@de.ibm.com>
2024-10-22 11:58:19 +0200

    metadata 3

--- 3_send_image_to_kafka.py
+++ 3_send_image_to_kafka.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 
-from confluent_kafka import Producer, KafkaError
-from confluent_kafka.admin import AdminClient, NewTopic
 import json
 import base64
+
+from astropy.io import fits
+from confluent_kafka import Producer, KafkaError
+from confluent_kafka.admin import AdminClient, NewTopic
 
 BROKER = 'watsonxdata:29092' 
 
@@ -39,14 +41,43 @@ def read_fits_image_as_base64(fits_image_path) :
     
     encoded_file_content = base64.b64encode(file_content).decode('utf-8')
 
-    return encoded_file_content
+    with fits.open(fits_image_path) as hdul:
+        image_header = hdul[0].header
 
-def send_file_image_to_kafka(producer, topic, file, image_base64):
+    return image_header, encoded_file_content
+
+def send_file_image_to_kafka(producer, topic, file, image_base64, header):
+
+    image_width = str(header['NAXIS1'])
+    image_height = str(header['NAXIS2'])
+    image_utz = header['UT-OBS']
+    object_name = header['OBJECT']
+    object_ra = str(header['RA'])
+    object_dec = str(header['DEC'])
+    object_alt =str( header['TELALT'])
+    object_az = str(header['TELAZ'])
+    camera_focus = str(header['CAMFOCUS'])
+    local_temp = str(header['TELTEMP'])
+    local_lat = str(header['LATITUDE'])
+    local_long = str(header['LONGITUD'])
+    local_weather = str(header['WEATHER'])
 
     event = {
-        'image_format': 'FITS',
-        'file': file,
-        'image_data': image_base64        
+        'file':         file,
+        'image_width':  image_width,
+        'image_height': image_height,
+        'image_utz':    image_utz,
+        'object_name':  object_name,
+        'object_ra':    object_ra,
+        'object_dec':   object_dec,
+        'object_alt':   object_alt,
+        'object_az':    object_az,
+        'camera_focus': camera_focus,
+        'local_temp':   local_temp,
+        'local_lat':    local_lat,
+        'local_long':   local_long,
+        'local_weather':local_weather,   
+        'image_data':   image_base64        
     }
     
     producer.produce(topic, key="fits_image", value=json.dumps(event), callback=delivery_report)
@@ -65,8 +96,8 @@ create_kafka_topic(topic)
 producer = create_kafka_producer()
 
 file_image_path = './images/m31dot.fits'  
-image_base64 = read_fits_image_as_base64(file_image_path)
+image_header, image_base64 = read_fits_image_as_base64(file_image_path)
 
-send_file_image_to_kafka(producer, topic, file_image_path, image_base64)
+send_file_image_to_kafka(producer, topic, file_image_path, image_base64, image_header)
 
 print(f'File sent to Kafka topic "{topic}".')
